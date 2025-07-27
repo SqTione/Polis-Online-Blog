98de188a58f1a4d69b0b143ad8430c5bf9a410fd
sqtione <sqtione@icloud.com>
2025-07-27 18:58:42 +0300

    Created article layout

---
+++ apps/web/src/entities/Article/index.ts
@@ -0,0 +1,2 @@
+export { Article } from './Article'
+export { ArticleCollapsed } from './ArticleCollapsed'
--- apps/web/src/widgets/Articles/Articles.tsx
+++ apps/web/src/widgets/Articles/Articles.tsx
@@ -1,8 +1,13 @@
+import { ArticleCollapsed } from '@entities/Article'
+
 export const Articles = () => {
 	return (
 		<section id='articles'>
 			<div className='container-xs md:container mx-auto'>
-				<h2>Статьи</h2>
+				<h2 className='mb-6'>Статьи</h2>
+				<div className='grid grid-flow-row gap-4'>
+					<ArticleCollapsed />
+				</div>
 			</div>
 		</section>
 	)
