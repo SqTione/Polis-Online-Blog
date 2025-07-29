3c440bc3cd291cdc41020bab9654cd33ac0807eb
sqtione <sqtione@icloud.com>
2025-07-29 13:18:14 +0300

    Created articles seeder

--- apps/api/app/Article.php
+++ apps/api/app/Article.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use App\Comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
+    use HasFactory;
+
     protected $table = "article";
 
     protected $fillable = [
